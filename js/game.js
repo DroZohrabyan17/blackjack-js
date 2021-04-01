74f582abff07c732ffc97168f40ce67b1948ee71
Gevorg Mkrtchyan <admin@armrus.net>
2021-04-01 15:55:06 +0300

    Add start game processing

--- js/game.js
+++ js/game.js
@@ -1,23 +1,23 @@
 let Game =  {
-  // Cards group.
-  kalod: [],
+  // Cards group. foramt: type:value
+  deck: [],
   // 1=> Sirt, 2 => Qyap, 3=> Khach, 4 => Ghar.
   cardsType: ['1', '2', '3', '4'],
   // Card balues.
-  valuse: {
-    '2' => 2,
-    '3' => 3,
-    '4' => 4,
-    '5' => 5,
-    '6' => 6,
-    '7' => 7,
-    '8' => 8,
-    '9' => 9,
-    '10' => 10,
-    'J' => 10,
-    'Q' => 10,
-    'K' => 10,
-    'T' => 11
+  values: {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'J': 10,
+    'Q': 10,
+    'K': 10,
+    'T': 11
   },
   playerMove: true,
 
@@ -28,7 +28,25 @@ let Game =  {
   dealerCards: [],
 
   //Generate initial state.
-  generate: function(){},
+  generate: function(){
+    let that = this;
+    // create deck.
+    for(let key in that.values){
+      that.cardsType.forEach((mast) => {
+        that.deck.push( mast + ':' + key);
+      });
+    }
+  },
+  // Parse cart nominal value.
+  parseCard: function(card){
+    let cardParts = card.split(':');
+    let cardDetails = {
+      type: cardParts[0],
+      key: cardParts[1],
+      value: this.values[cardParts[1]]
+    }
+    return cardDetails;
+  },
   // Next step.
   next: function(){},
   // Dealer need move or note?
@@ -40,5 +58,27 @@ let Game =  {
 
   nextBtnClick: function(){},
   stopBtnClick: function(){},
-  startGame: function(){},
+
+  // Calculate
+  calculate: function(){
+
+  },
+  //Gett random card from deck.
+  getCard: function(){
+    let that = this;
+    let itemKey = Math.floor(Math.random() * that.deck.length);
+    let cards = that.deck.splice(itemKey, 1);
+    return that.parseCard(cards.pop());
+  },
+
+  startGame: function(){
+    let that = this;
+    that.generate();
+
+    that.playerCards.push(that.getCard());
+    that.playerCards.push(that.getCard());
+
+    that.dealerCards.push(that.getCard());
+
+  },
 }
